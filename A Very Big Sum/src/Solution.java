b791f28d6793b5be92761c150b72815e009bb955
Denis Konev <deniskonev1984@gmail.com>
2022-07-15 17:39:19 +0200

    "A Very Big Sum" moved to correct directory

--- A Very Big Sum/src/Solution.java
+++ A Very Big Sum/src/Solution.java
@@ -1,8 +1,7 @@
 import java.io.*;
 import java.util.*;
-import java.util.stream.*;
+
 import static java.util.stream.Collectors.joining;
-import static java.util.stream.Collectors.toList;
 
 class Result {
 
