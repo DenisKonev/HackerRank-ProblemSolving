2ebef34e84483736467db09aeeef80808aefda7a
Denis Konev <deniskonev1984@gmail.com>
2022-08-14 14:23:52 +0200

    "Find Digits" solved

---
+++ Find Digits/src/Solution.java
@@ -0,0 +1,74 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'findDigits' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts INTEGER n as parameter.
+     */
+
+    public static int findDigits(int n) {
+        ArrayList<Integer> array = new ArrayList<Integer>(makeIntList(n));
+        int result = 0;
+
+        for (int elem : array) {
+            if (elem == 0) {
+                continue;
+            }
+            if (n % elem == 0) {
+                result++;
+            }
+        }
+        return result;
+    }
+
+    private static Collection<Integer> makeIntList(int number) {
+        int temp = number;
+        ArrayList<Integer> array = new ArrayList<Integer>();
+
+        do{
+            array.add(temp % 10);
+            temp /= 10;
+        } while  (temp > 0);
+        Collections.reverse(array);
+        return array;
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int t = Integer.parseInt(bufferedReader.readLine().trim());
+
+        IntStream.range(0, t).forEach(tItr -> {
+            try {
+                int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+                int result = Result.findDigits(n);
+
+                bufferedWriter.write(String.valueOf(result));
+                bufferedWriter.newLine();
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
