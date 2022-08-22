1c0af15183cd40d4e6260c2ca7e831772695140f
Denis Konev <deniskonev1984@gmail.com>
2022-08-22 11:48:18 +0200

    "Minimum Distances" solved

---
+++ Minimum Distances/src/Solution.java
@@ -0,0 +1,61 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'minimumDistances' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts INTEGER_ARRAY a as parameter.
+     */
+
+    public static int minimumDistances(List<Integer> a) {
+        int result = a.size();
+        Set<Integer> arrSet = new TreeSet<Integer>(a);
+
+        for (int elem : arrSet) {
+            int firstElemIndex = a.indexOf(elem), lastElemIndex = a.lastIndexOf(elem);
+            if ((firstElemIndex != -1) && (lastElemIndex != -1) && (firstElemIndex != lastElemIndex)) {
+                int distance = lastElemIndex - firstElemIndex;
+                if (result > distance)
+                    result = distance;
+            }
+        }
+        if (result == a.size())
+            return -1;
+        else
+            return result;
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> a = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                .map(Integer::parseInt)
+                .collect(toList());
+
+        int result = Result.minimumDistances(a);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
